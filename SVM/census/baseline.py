527a1f2417cedf0df100eab9cdd6b6511e024981
emarutherford <40407963+emarutherford@users.noreply.github.com>
2018-10-24 00:40:00 +0100

    Add files via upload

---
+++ SVM/census/baseline.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+from sklearn.cross_validation import train_test_split
+from sklearn import svm
+from sklearn import metrics
+
+#code for importing and pre processing data from https://www.kaggle.com/bananuhbeatdown/multiple-ml-techniques-and-analysis-of-dataset
+#dataset itself a truncated version of dataset from https://www.kaggle.com/uciml/adult-census-income
+path = 'census_data.csv'
+data = pd.read_csv(path)
+data = data[data.occupation != '?']
+raw_data = data[data.occupation != '?']
+
+data['workclass_num'] = data.workclass.map({'Private':0, 'State-gov':1, 'Federal-gov':2, 'Self-emp-not-inc':3, 'Self-emp-inc':4, 'Local-gov':5, 'Without-pay':6})
+data['over50K'] = np.where(data.income == '<=50K', 0, 1)
+data['marital_num'] = data['marital.status'].map({'Widowed':0, 'Divorced':1, 'Separated':2, 'Never-married':3, 'Married-civ-spouse':4, 'Married-AF-spouse':4, 'Married-spouse-absent':5})
+data['race_num'] = data.race.map({'White':0, 'Black':1, 'Asian-Pac-Islander':2, 'Amer-Indian-Eskimo':3, 'Other':4})
+data['sex_num'] = np.where(data.sex == 'Female', 0, 1)
+data['rel_num'] = data.relationship.map({'Not-in-family':0, 'Unmarried':0, 'Own-child':0, 'Other-relative':0, 'Husband':1, 'Wife':1})
+data.head()
+X = data[['workclass_num', 'education.num', 'marital_num', 'race_num', 'sex_num', 'rel_num', 'capital.gain', 'capital.loss']]
+y = data.over50K
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=42)
+clf = svm.SVC(kernel='linear')
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test);
+
+print("*****CENSUS BASELINE DATA, kernel=linear*****")
+print("accuracy: ", metrics.accuracy_score(y_test,y_pred))
+print("Precision:",metrics.precision_score(y_test, y_pred))
+print("Recall:",metrics.recall_score(y_test, y_pred))
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=42)
+clf = svm.SVC(kernel='rbf')
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test);
+
+print("*****CENSUS BASELINE DATA, kernel=rbf*****")
+print("accuracy: ", metrics.accuracy_score(y_test,y_pred))
+print("Precision:",metrics.precision_score(y_test, y_pred))
+print("Recall:",metrics.recall_score(y_test, y_pred))
+
---
+++ SVM/census/randomSearch.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+
+from sklearn import svm
+from sklearn.datasets import load_breast_cancer
+from sklearn import metrics
+from sklearn.cross_validation import train_test_split
+from sklearn.grid_search import RandomizedSearchCV
+
+#code for importing and pre processing data from https://www.kaggle.com/bananuhbeatdown/multiple-ml-techniques-and-analysis-of-dataset
+#dataset itself a truncated version of dataset from https://www.kaggle.com/uciml/adult-census-income
+path = 'census_data.csv'
+data = pd.read_csv(path)
+data = data[data.occupation != '?']
+raw_data = data[data.occupation != '?']
+
+print("*****CENSUS RANDOMIZED SEARCH*****")
+data['workclass_num'] = data.workclass.map({'Private':0, 'State-gov':1, 'Federal-gov':2, 'Self-emp-not-inc':3, 'Self-emp-inc':4, 'Local-gov':5, 'Without-pay':6})
+data['over50K'] = np.where(data.income == '<=50K', 0, 1)
+data['marital_num'] = data['marital.status'].map({'Widowed':0, 'Divorced':1, 'Separated':2, 'Never-married':3, 'Married-civ-spouse':4, 'Married-AF-spouse':4, 'Married-spouse-absent':5})
+data['race_num'] = data.race.map({'White':0, 'Black':1, 'Asian-Pac-Islander':2, 'Amer-Indian-Eskimo':3, 'Other':4})
+data['sex_num'] = np.where(data.sex == 'Female', 0, 1)
+data['rel_num'] = data.relationship.map({'Not-in-family':0, 'Unmarried':0, 'Own-child':0, 'Other-relative':0, 'Husband':1, 'Wife':1})
+data.head()
+X = data[['workclass_num', 'education.num', 'marital_num', 'race_num', 'sex_num', 'rel_num', 'capital.gain', 'capital.loss']]
+y = data.over50K
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+parameters = {'kernel': ('linear', 'rbf'), 'C':[0.001,0.01,0.1,1,10],'gamma':[0.001,0.01,0.1,1]}
+svc = svm.SVC()
+clf = RandomizedSearchCV(svc,parameters,n_iter=40)
+clf.fit(X_train,y_train)
+print(clf.best_params_)
+y_pred = clf.predict(X_test);
+
+print("accuracy: ", metrics.accuracy_score(y_test,y_pred))
+print("Precision:",metrics.precision_score(y_test, y_pred))
+print("Recall:",metrics.recall_score(y_test, y_pred))
