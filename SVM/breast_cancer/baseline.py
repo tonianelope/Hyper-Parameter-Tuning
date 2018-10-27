f6ecaf395e538fd4a1effc667c92c10e504c113c
emarutherford <40407963+emarutherford@users.noreply.github.com>
2018-10-27 23:04:56 +0100

    Update baseline.py

--- SVM/breast_cancer/baseline.py
+++ SVM/breast_cancer/baseline.py
@@ -5,19 +5,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 import timeit
 
-start = timeit.default_timer()
 data = load_breast_cancer()
 X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.3, random_state=142)
 
+start = timeit.default_timer()
 clf = svm.SVC(kernel='linear')
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test);
+stop = timeit.default_timer()
 
 print("*****BREAST CANCER BASELINE, kernel=linear*****")
 print("accuracy: ", metrics.accuracy_score(y_test,y_pred))
 print("Precision:",metrics.precision_score(y_test, y_pred))
 print("Recall:",metrics.recall_score(y_test, y_pred))
+print('Time: ', stop - start)
 
+start = timeit.default_timer()
 clf = svm.SVC(kernel='rbf')
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test);
