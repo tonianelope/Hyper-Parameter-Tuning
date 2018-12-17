7994c64fa112e370687fdc9e501cf6001b7811d5
emarutherford <40407963+emarutherford@users.noreply.github.com>
2018-12-17 15:14:36 +0000

    Update bayes.py

--- SVM/fashion/bayes.py
+++ SVM/fashion/bayes.py
@@ -4,7 +4,7 @@ import mnist_reader
 from skopt import BayesSearchCV
 from skopt.space import Real, Categorical
 from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 import timeit
 
 class BayesSearchCV(BayesSearchCV):
@@ -16,6 +16,9 @@ X_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k')
 
 X_train = X_train[:10000]
 y_train = y_train[:10000]
+scaler = preprocessing.StandardScaler().fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 
 start = timeit.default_timer()
 opt = BayesSearchCV(
@@ -27,10 +30,12 @@ opt = BayesSearchCV(
     },
     n_iter=40
 )
+scores = cross_val_score(opt, X_train, y_train, cv=5)
 opt.fit(X_train, y_train)
 stop = timeit.default_timer()
 
 print("Bayes on fashion data with kernel poly")
 print(opt.score(X_test, y_test))
 print(opt.best_params_)
+print("Accuracy: (CrossVal) %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 print('Time: ', stop - start)  
