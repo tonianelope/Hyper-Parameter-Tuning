60d04668efd6cf0694d73a7ac43a48c39fd3f7a6
emarutherford <40407963+emarutherford@users.noreply.github.com>
2018-12-17 12:03:41 +0000

    Update gs.py

--- SVM/fashion/gs.py
+++ SVM/fashion/gs.py
@@ -14,6 +14,9 @@ X_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k')
 
 X_train = X_train[:10000]
 y_train = y_train[:10000]
+scaler = preprocessing.StandardScaler().fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 
 print("Grid Search for fashion data with kernel poly")
 
@@ -21,10 +24,12 @@ start = timeit.default_timer()
 parameters = {'C':[0.001,0.01,0.1,1,10],'gamma':[0.001,0.01,0.1,1]}
 svc = svm.SVC(kernel='poly')
 clf = GridSearchCV(svc,parameters)
+scores = cross_val_score(clf, X_train, y_train, cv=5)
 clf.fit(X_train,y_train)
 print(clf.best_params_)
 y_pred = clf.predict(X_test);
 stop = timeit.default_timer()
 
 print("accuracy: ", metrics.accuracy_score(y_test,y_pred))
+print("Accuracy: (CrossVal) %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 print("Time: ", stop - start)
