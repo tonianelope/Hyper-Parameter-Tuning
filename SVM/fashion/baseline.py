3ae73564ac1bba68dfbbb96f643c99d053c93581
emarutherford <40407963+emarutherford@users.noreply.github.com>
2018-12-17 12:01:26 +0000

    Update baseline.py

--- SVM/fashion/baseline.py
+++ SVM/fashion/baseline.py
@@ -10,10 +10,14 @@ X_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k')
 
 X_train = X_train[:10000]
 y_train = y_train[:10000]
+scaler = preprocessing.StandardScaler().fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 
 start = timeit.default_timer()
 kernel_name = 'poly'
 clf = svm.SVC(kernel=kernel_name)
+scores = cross_val_score(clf, X_train, y_train, cv=5)
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 stop = timeit.default_timer()
@@ -21,4 +25,5 @@ stop = timeit.default_timer()
 
 print("*****FASHION BASELINE DATA with kernel=", kernel_name, "*****")
 print("accuracy: ", metrics.accuracy_score(y_test,y_pred))
+print("Accuracy: (CrossVal) %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 print("Time: ", stop - start)
