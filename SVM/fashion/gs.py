e6815118af07bdd269e7a58d970250925551d047
emarutherford <40407963+emarutherford@users.noreply.github.com>
2018-12-17 12:18:57 +0000

    Update gs.py

--- SVM/fashion/gs.py
+++ SVM/fashion/gs.py
@@ -4,7 +4,7 @@ import mnist_reader
 from sklearn import svm
 from sklearn.datasets import load_breast_cancer
 from sklearn import metrics
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.model_selection import GridSearchCV
 from sklearn import preprocessing
 import timeit
